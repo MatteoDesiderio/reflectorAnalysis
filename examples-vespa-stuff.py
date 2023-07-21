bcaf3a375634ea5819f2dd5fbf4a30313cc54ca6
MatteoDesiderio <4matteo.desiderio@gmail.com>
2023-07-21 12:58:23 +0100

    Add plot to debug/visualize the procedure of choosing a reference distance. Will incorporate this in the classes later on.

--- examples-vespa-stuff.py
+++ examples-vespa-stuff.py
@@ -9,26 +9,42 @@ Created on Thu Jul 20 16:53:54 2023
 from vespa import Section, Phases
 import matplotlib.pyplot as plt
 import numpy as np
+plt.ion()
 
 # %%
 sec = Section("runC201509130814A/", "PR")
-#sec.collect_aligned() # to compute from the top
+sec.collect_aligned() # to compute from the top
 sec.save()
 time, data, _, _ = sec.to_numpy_data()
-sec.plot_data(time, data)
 
 # %%
-reflector_depths = np.arange(100, 3000, 100)
+reflector_depths = np.arange(0, 1600, 100)
 phs = Phases("runC201509130814A/", "PR", "reflectors-coarse", reflector_depths)
 
-phs.get_precursors()
+phs.get_precursors(at_reference=False)
 
 # %%
 phs.get_slownesses_t_precursors()
 
 # %%
-
+s = phs.slowness_precursors
+t = phs.time_precursors
+d = phs.distances
 ref_i = np.argmin(np.abs(phs.d_reference - np.r_[phs.distances]))
-d = phs.distances[ref_i]
-s = phs.slowness_precursors[:, ref_i]
-t = phs.time_precursors[:, ref_i]
+s_line = s[:, ref_i]
+t_line = t[:, ref_i] 
+
+# %%
+sec.plot_data(time, data)
+plt.figure()
+plt.plot((t-sec.ttimes).T , d, "b")
+tt = (t - sec.ttimes)[:, ref_i] 
+s_ref = s_line[0]
+cc=0
+for t0, slowness in zip(tt, s_line):
+    if slowness > 0:
+        plt.plot( (d - phs.d_reference) * (slowness -  s_ref) + t0, d, "r")
+        cc+=1
+plt.xlim((-400, 40))
+plt.ylim((d[-1], d[0]))
+
