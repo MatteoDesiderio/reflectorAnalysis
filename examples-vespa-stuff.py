Fix bug in cross section calculation and plotting

examples-vespa-stuff.py
@@ -12,16 +12,18 @@ import numpy as np
 plt.ion()
 plt.style.use("ggplot")
 
+case = "PP"
+
 # %%
-sec = Section("runC201509130814A/", "PR")
+sec = Section("runC201509130814A/", case)
 sec.collect_aligned() # to compute from the top
 sec.save()
 time, data, _, _ = sec.to_numpy_data()
 
 # %%
-reflector_depths = np.arange(0, 1600, 100)
-phs = Phases("runC201509130814A/", "PR", "reflectors-coarse", reflector_depths,
-             d_reference=138)
+reflector_depths = np.arange(0, 1500, 5)
+phs = Phases("runC201509130814A/", case, "SdS_0.0_2.5.2", reflector_depths,
+             d_reference=125)
 
 # %%
 phs.get_precursors(at_reference=True)
@@ -29,8 +31,7 @@ phs.get_slownesses_t_precursors()
 
 # %%
 sec.plot_data(time, data)
-phs.plot_precursors(True, color="crimson", 
-                    args_ref_plot={"color":"w", "ls":"--"})
+#phs.plot_precursors(True, color="r", args_ref_plot={"color":"w", "ls":"--"})
 plt.xlim((-440, 40))
 
 # %%
@@ -53,9 +54,18 @@ ax1.set_ylim([slownesses.max(), slownesses.min()])
 ax2.set_xlabel("Time - SS peak time [s]")
 ax1.set_ylabel("Slowness [s/deg]")
 ax1.plot(*phs.line_reduced.T, "k:")
+
+ax1.axvline(*phs.line_reduced[reflector_depths==1000].squeeze())
+ax1.axvline(*phs.line_reduced[reflector_depths==660].squeeze())
+ax1.axvline(*phs.line_reduced[reflector_depths==410].squeeze())
+ax1.axvline(-216)
 #plt.title("Vespagram for %s" % titles[case])
 
-#%%
-w = Vespagram.cross_section(phs.line_reduced, delays, slownesses, vespagram)
-ax2.plot(delays, w, "k")
+# %%
+x, w = Vespagram.cross_section(phs.line_reduced, delays, slownesses, vespagram)
+ax2.plot(x, w, "k")
 ax2.set_ylim([vmin, vmax])
+ax2.axvline(*phs.line_reduced[reflector_depths==1000].squeeze())
+ax2.axvline(*phs.line_reduced[reflector_depths==660].squeeze())
+ax2.axvline(*phs.line_reduced[reflector_depths==410].squeeze())
+ax2.axvline(-216)

vespa.py
@@ -111,21 +111,29 @@ class Vespagram:
     
     @staticmethod
     def cross_section(line, delays, slownesses, vespagram):
+        # t and slown. corresp. to the reference distance
         t, s = line.T
+        
         s_interp = np.interp(delays, t[::-1], s[::-1])
+        # make vespagram equal along both directions (square image)
         inerpol8r = interp1d(s, vespagram, axis=0)
-        v_interp = inerpol8r(s_interp)
+        snew = np.linspace(slownesses.min(), slownesses.max(), 
+                           len(delays))
+        v_interp = inerpol8r(snew)
         
-        x, y = s_interp  - s_interp.min(), delays - delays.min()
-        x = x / x.max() * len(x) - 1
-        y = y / y.max() * len(y) - 1
-        
+        # convert ref s and t to pixel coordinates
+        y, x = s_interp  - s_interp.min(), delays - delays.min()
+        x = np.rint((x / x.max()) * (len(x) - 1))
+        y = np.rint((y / y.max()) * (len(y) - 1))
         i, j = y.astype(np.int), x.astype(np.int)
+        # extract pixels along reference s and t line
+        w = v_interp[i, j]
+        delays_w = delays[j]
+        # delete values before min reference t
+        interval = delays_w >= t.min()   
+        return delays_w[interval], w[interval]
         
-        w = v_interp[j, i]
-            
-        return w
-
+    
     def compute(self):
         self.prepare()
         time = self.time
@@ -459,7 +467,6 @@ class Phases:
         
         if at_reference:
             distances = [self.d_reference]
-            print("ah!")
         else:
             distances = self.distances
         
@@ -493,10 +500,6 @@ class Phases:
                     precursors.append(np.nan)
                     
             self.precursors.append(arrivals)
-                    
-        # wont work w getter and setters 
-        # if you dont change from list to array
-        # self.precursors = precursors 
         
     def get_slownesses_t_precursors(self):
         precursors = self.precursors